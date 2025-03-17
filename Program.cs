a7ff5d43deb6e2d096e4502b5fdf1caafa4ef789
Beatriz Batista <byah24@outlook.com>
2025-03-17 15:37:51 -0300

    Programa 80% finalizado

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Common;
+
+Console.Clear();
+
+string PalavraSecreta = " ";
+char palpite = 'x';
+
+Console.WriteLine("###Jogo da Forca###");
+
+Console.WriteLine("Digite a palavra secreta desejada:");
+
+ PalavraSecreta = Console.ReadLine()!;
+
+Console.WriteLine("Digite a letra que você acha que tem:");
+palpite = char.Parse(Console.ReadLine());
+
+Console.WriteLine($"A letra {palpite} {(PalavraSecreta.Contains(palpite) ? "existe" : "não existe")} na palavra {PalavraSecreta}");
+
+Console.ReadKey(true);
