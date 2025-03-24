e0f71319c7cf91b3ff14f5001d18666e46bf9b6f
Beatriz Batista <byah24@outlook.com>
2025-03-24 10:32:03 -0300

    Pograma Finalizado para a lista 2

--- Program.cs
+++ Program.cs
@@ -12,7 +12,10 @@ Console.WriteLine("Digite a palavra secreta desejada:");
  PalavraSecreta = Console.ReadLine()!;
 
 Console.WriteLine("Digite a letra que você acha que tem:");
-palpite = char.Parse(Console.ReadLine());
+palpite = char.Parse(Console.ReadLine()!.ToLower());
+
+//Console.ReadKey().KeyChar; 
+//Também é possível utilizar comando acima ao invés do comando abaixo char.Parse....
 
 Console.WriteLine($"A letra {palpite} {(PalavraSecreta.Contains(palpite) ? "existe" : "não existe")} na palavra {PalavraSecreta}");
 
